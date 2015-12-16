2f1754b079166c81812daa1878c79a8a1d40768d
tyler <trobbi11@uncc.edu>
2015-12-16 18:22:11 -0500

    new 3

--- src/NW.java
+++ src/NW.java
@@ -47,24 +47,53 @@ public class NW {
 		//}
 		
 		// Initialize top left cell
-		cell[0][0] = new Cell("FIRST",0);
+		cell[0][0] = new Cell("first",0);
 		
 		// Fill in gap penalties
 		for (int x=1; x <= seq1.length(); x++){
-			cell[x][0] = new Cell("LEFT",-8*x);
+			cell[x][0] = new Cell("left",-8*x);
 			System.out.print(cell[x][0].score);
 		}
 		System.out.println();
 		for (int x=1; x <= seq2.length(); x++){
-			cell[0][x] = new Cell("UP",-8*x);
+			cell[0][x] = new Cell("up",-8*x);
 			System.out.println(cell[0][x].score);
 			//seqMatrix[x][0] = x * -8;
 		}
 		System.out.println();
 		
 		// fill in scoring matrix
+		int up;
+		int left;
+		int diagonal;
+		int max;
 		for(int y = 1; y < seq2.length(); y++){
 			for(int x = 1; x < seq1.length(); x++){
+				up = cell[x][y-1].score - 8;
+				left = cell[x-1][y].score - 8;
+				diagonal = cell[x-1][y-1].score + matrix[residues.indexOf(seq1Array[x-1])][residues.indexOf(seq2Array[y-1])];
+				max = 0;
+				int maxArray[] = {up,left,diagonal};
+				
+				// determine a maximum value
+				for(int i = 0; i < 3; i++){
+					if(maxArray[i] > max){
+						max = maxArray[i];
+					}
+				}
+				
+				Cell maxAlign = new Cell(max);
+				
+				// set direction for maximum alignment
+				if(max==up){
+					maxAlign.dir = "up";
+				}else if(max==left){
+					maxAlign.dir = "left";
+				}else{
+					maxAlign.dir = "diagonal";
+				}
+				
+				cell[x][y] = maxAlign;
 				//System.out.println(residues.indexOf(seq2Array[y]));
 				//seqMatrix[x][y] = matrix[residues.indexOf(seq1Array[x-1])][residues.indexOf(seq2Array[y-1])];
 				
@@ -82,13 +111,13 @@ public class NW {
 		
 	}
 	private static class Cell{
-		String direction;
-		float score;
+		String dir;
+		int score;
 		public Cell(int score){
 			this.score = score;
 		}
-		public Cell(String direction,float score){
-			this.direction = direction;
+		public Cell(String dir,int score){
+			this.dir = dir;
 			this.score = score;
 		}
 	}
